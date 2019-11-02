71c72547d3e98e06ff0d0091a0b3ed18ccb3afdb
Riley Jones <rjone159@uncc.edu>
2019-11-02 12:35:34 -0400

    Player turnaround sprite, speed changes & jump adjustments

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,6 +18,7 @@ onready var state_machine: Node = $StateMachine
 onready var jump_grace: Timer = $JumpGrace
 
 var prev_on_floor = false
+var x_input = 0
 
 func default_movement(delta: float) -> void:
 	# Start the gravity with some speed so that collision with floor is always detected
@@ -26,7 +27,7 @@ func default_movement(delta: float) -> void:
 	
 	velocity.y += delta * GRAVITY
 	
-	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if abs(velocity.x) < MAX_X_SPEED:
 		velocity.x += x_input * delta * ACCELERATION
@@ -48,7 +49,7 @@ func default_movement(delta: float) -> void:
 	prev_on_floor = is_on_floor()
 	
 	if is_on_floor() or (jump_grace.time_left != 0 and velocity.y >= 0):
-		if Input.is_action_pressed("ui_select"):
+		if Input.is_action_just_pressed("ui_select"):
 			jump()
 	
 	if not is_on_floor() and is_on_wall():
@@ -60,20 +61,25 @@ func default_movement(delta: float) -> void:
 		sprite.offset.x = sign(velocity.x) * 16
 		sprite.flip_h = velocity.x < 0
 		
-	if get_slide_count() == 0 and velocity.y > 0:
-		var snap = Vector2(WALL_SNAP_DIST, 0) * Vector2(sign(velocity.x), 1)
+	if velocity.y>0 && get_slide_count() == 0:
+		var snap = Vector2(WALL_SNAP_DIST, 0) * (-1 if sprite.flip_h else 1)
+		#	Vector2(sign(velocity.x), 1)
+			
 		if test_move(transform, snap):
 			if Input.is_action_just_pressed("ui_select"):
 				# Not multiplied by delta because this is the real distance to move
 				move_and_collide(snap)
-				state_machine.set_deferred("current_state", "StateOnWall")
+				#state_machine.set_deferred("current_state", "StateOnWall")
+				jump(true)
 		
 func default_animation(delta: float) -> void:
 	if velocity.y == 0:
 		if velocity.x == 0:
 			anim_player.play("player_idle")
+		elif sign(velocity.x) != sign(x_input) && sign(x_input) != 0:
+			anim_player.play("player_turnAround")
 		else:
-			anim_player.play("player_run")
+			anim_player.play("player_run", -1, velocity.x/MAX_X_SPEED)
 	else:
 		if velocity.y > 0:
 			anim_player.play("player_air_down")
@@ -85,7 +91,7 @@ func default_collisions() -> void:
 		for i in range(get_slide_count()):
 			if get_slide_collision(i).collider.is_in_group("enemies"):
 				jump()
-		
+
 func jump(off_wall = false) -> void:
 	jump_grace.stop()
 	
