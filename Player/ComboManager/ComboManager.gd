b22ca7042302d7f89fb0682fb819c335acdb7531
Luke <lsloop4@uncc.edu>
2019-11-03 18:12:18 -0500

    Update

--- Player/ComboManager/ComboManager.gd
+++ Player/ComboManager/ComboManager.gd
@@ -5,20 +5,22 @@ enum Combo {
 }
 
 var _combo_log = []
-var combo_points = 0
+var combo_points = 0 setget set_combo_points
 
 func add_combo(i: int) -> void:
 	_combo_log.append(i)
 	var size = len(_combo_log)
 	$AnimationPlayer.play("combo_grow")
-	combo_points += size
+	self.combo_points += 1
 	get_owner().combo_label.text = "Combo *%d" % size
+
+func set_combo_points(to: int) -> void:
 	get_owner().combo_points_label.text = str(combo_points)
+	combo_points = to
 
 func reset_combo() -> void:
-	if _combo_log:
+	if combo_points > 0:
 		_combo_log.clear()
-		get_owner().collectables += combo_points
-		combo_points = 0
+		get_owner().collectables += combo_points * len(_combo_log)
+		self.combo_points = 0
 		get_owner().combo_label.text = "Combo *%d" % len(_combo_log)
-		get_owner().combo_points_label.text = str(combo_points)
